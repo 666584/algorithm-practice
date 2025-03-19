0b69730a1397201d4aa11037e9e37ce6aa27b05f
Sussie <yurimc@student.unimelb.edu.au>
2025-03-19 14:17:11 +1100

    Use break instead of continue

--- 2025/271A_Beautiful_Year.py
+++ 2025/271A_Beautiful_Year.py
@@ -7,7 +7,7 @@ while output == 0:
     for n in str(next_y):
         if str(next_y).count(n) != 1:
             next_y = next_y + 1
-            continue
+            break
         else:
             if str(next_y).index(n) == 3:
                 output = next_y
