402865f32eb4fe9be0d06df566f377695c02fad3
Sussie <yurimc@student.unimelb.edu.au>
2025-03-19 14:13:40 +1100

    Create 271A_Beautiful_Year.py

---
+++ 2025/271A_Beautiful_Year.py
@@ -0,0 +1,16 @@
+# 271A
+# Beautiful Year
+y = int(input())
+next_y = y + 1
+for n in str(next_y):
+    print(n)
+output = 0
+while output == 0:
+    for n in str(next_y):
+        if str(next_y).count(n) != 1:
+            next_y = next_y + 1
+            continue
+        else:
+            if str(next_y).index(n) == 3:
+                output = next_y
+                print(output)
