9e5a2c113b1d6b69950eca01f5c3af35f17c675c
Sussie <yurimc@student.unimelb.edu.au>
2025-07-13 12:24:20 +1000

    10798

--- 백준/브론즈/10798.py
+++ 백준/브론즈/10798.py
@@ -1,11 +1,22 @@
 """
-가로 세로 5*5의 문자, 숫자열을 세로로 읽는다. 
+가로 세로 15(최소 1, 최대 15)*5의 문자, 숫자열을 세로로 읽는다. 
 
 작성일: 2025-07-13
-메모리: 32412 KB
-시간: 32ms
+메모리: 32544 KB
+시간: 48ms
 """
 def read_vertically():
-    for i in range(0,5):
-        for j in range(0,5):
-            
+    sentence_list = []
+    count_list = []
+    for _ in range(0,5):
+        word = input()
+        sentence_list.append(word)
+        count_word = len(word)
+        count_list.append(count_word)
+    sentence = ''
+    for i in range(0,15):
+        for j in range(0,5): 
+            if i < count_list[j]:   
+                sentence = sentence + sentence_list[j][i]
+    print(sentence)
+read_vertically()
