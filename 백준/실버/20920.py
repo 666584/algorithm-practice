4b42fec2b57c5d9a7e8c200ae6d6b4b1a303d973
Sussie <yurimc@student.unimelb.edu.au>
2025-07-13 16:52:13 +1000

    Refactoring

--- 백준/실버/20920.py
+++ 백준/실버/20920.py
@@ -6,30 +6,18 @@
 from collections import Counter
 n, m = map(int, input().split())
 word_list = []
-for _ in range(n):
-    word_list.append(input())
-c = Counter(word_list)
-c = c.most_common()
-new_dict = {}
-for word in c:
-    if len(word[0]) >= m:
-        new_dict[word[0]] = word[1]
 
-count = 0
-sort_dict = {} 
-for word in new_dict:
-    value = new_dict[word]
-    if count != value:  
-        sort_dict[value]= [word]
-        count = value
-    else: 
-        sort_dict[value].append(word)
+words = [input() for _ in range(n)]
 
-for word in sort_dict:
-    if len(sort_dict[word]) == 1:
-        print(sort_dict[word][0])
-    else: 
-        sort_dict[word].sort()
-        sort_dict[word].sort(key=len, reverse = True)
-        for new_word in sort_dict[word]:
-            print(new_word)
+filtered_words = [word for word in words if len(word) >= m]
+
+word_counts = Counter(filtered_words)
+
+# 중요 부분
+sorted_words = sorted(
+    word_counts.items(),
+    key=lambda x: (-x[1], -len(x[0]), x[0])
+)
+
+for word, _ in sorted_words:
+    print(word)
