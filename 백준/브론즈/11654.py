4a12ff451109bcfcc6181449d938ef11c224f83e
Sussie <yurimc@student.unimelb.edu.au>
2025-07-14 21:56:21 +1000

    refactoring

--- 백준/브론즈/11654.py
+++ 백준/브론즈/11654.py
@@ -1,15 +1,8 @@
 """
+아스키코드 출력
 작성일: 2025-07-14
 메모리: 108384 KB
 시간: 	120 ms
 """
 import sys
-c = sys.stdin.readline().strip()
-big_char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-small_char_list = "abcdefghijklmnopqrstuvwxyz"
-if c.isdigit():
-    print(int(c) + 48)
-elif c in big_char_list:
-    print(big_char_list.find(c) + 65)
-elif c in small_char_list:
-    print(small_char_list.find(c) + 97)
+print(ord(sys.stdin.readline().strip()))
