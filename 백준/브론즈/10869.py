76949476c20164c92bee90f76e9ab62f0ec10b7b
Sussie <yurimc@student.unimelb.edu.au>
2025-07-15 11:59:52 +1000

    10869

---
+++ 백준/브론즈/10869.py
@@ -0,0 +1,12 @@
+"""
+작성일: 2025-07-15
+메모리: 108384 KB
+시간: 	116 ms
+"""
+import sys
+n1, n2 = map(int, sys.stdin.readline().split())
+print(n1+n2)
+print(n1-n2)
+print(n1*n2)
+print(int(n1/n2))
+print(n1%n2)
