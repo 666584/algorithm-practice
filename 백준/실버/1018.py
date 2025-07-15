bc371457c461176d523f0534ec97c557d1d8b0fb
Sussie <yurimc@student.unimelb.edu.au>
2025-07-15 15:59:44 +1000

    refactoring

--- 백준/실버/1018.py
+++ 백준/실버/1018.py
@@ -11,52 +11,22 @@ for _ in range(n):
 
 def find_min_num_color():
     min_num_color = float('inf') 
-    for i in range(0, n - 7):
-        for j in range(0, m - 7):
-            w_num_color = find_num_color_W(i, j)
-            b_num_color = find_num_color_B(i, j)
+    for i in range(n - 7):
+        for j in range(m - 7):
+            w_num_color = find_num_color(i, j, 'W')
+            b_num_color = find_num_color(i, j, 'B')
             new = min(w_num_color, b_num_color)
             if min_num_color > new:
                 min_num_color = new
     print(min_num_color)
 
-# 시작이 W일때
-def find_num_color_W(r, c):
-    num_color = 0
+def find_num_color(r, c, first_color):
+    expected = ['W', 'B'] if first_color == 'W' else ['B', 'W']
+    count = 0
     for i in range(8):
         for j in range(8):
-            # 홀수의 경우
-            if i%2 != 0:
-                if j%2 != 0:
-                    if color_list[i+r][j+c] != "W":
-                        num_color += 1
-                elif color_list[i+r][j+c] != "B":
-                    num_color += 1
-            else:
-                if j%2 != 0:
-                    if color_list[i+r][j+c] != "B":
-                        num_color += 1
-                elif color_list[i+r][j+c] != "W":
-                    num_color += 1
-    return num_color
-
-# 시작이 B일때
-def find_num_color_B(r, c):
-    num_color = 0
-    for i in range(8):
-        for j in range(8):
-            if i%2 != 0:
-                if j%2 != 0:
-                    if color_list[i+r][j+c] != "B":
-                        num_color += 1
-                elif color_list[i+r][j+c] != "W":
-                    num_color += 1
-            else:
-                if j%2 != 0:
-                    if color_list[i+r][j+c] != "W":
-                        num_color += 1
-                elif color_list[i+r][j+c] != "B":
-                    num_color += 1
-    return num_color
+            if color_list[r+i][c+j] != expected[(i+j)%2]:
+                count += 1
+    return count
 
 find_min_num_color()
