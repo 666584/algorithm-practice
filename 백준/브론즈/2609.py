635dd7af051e7e8047e4592f6d47501f4b6cfbb2
Sussie <yurimc@student.unimelb.edu.au>
2025-07-16 11:30:22 +1000

    최대공약수 최소공배수

---
+++ 백준/브론즈/2609.py
@@ -0,0 +1,22 @@
+"""
+작성일: 2025-07-16
+메모리: 108384 KB
+시간: 	100 ms
+"""
+import sys
+a, b = map(int, sys.stdin.readline().split())
+x = 1
+for i in range(1, min(a,b)+1):
+    if a % i == 0 and b % i == 0:
+        new_x = i
+        x = max(x, i)
+print(x)
+j = 1
+while True:
+    if (b * j) % a == 0:
+        print(b * j)
+        break
+    elif (a * j) % b == 0:
+        print(a * j)
+        break
+    j += 1
