f5d30e09bd748158c3f5d7db594674232eca7c5b
Sussie <yurimc@student.unimelb.edu.au>
2025-07-25 13:42:31 +1000

    bfs

--- 백준/실버/1012.py
+++ 백준/실버/1012.py
@@ -1,38 +1,48 @@
 """
-dfs 순서
-작성일: 2025-07-19
-메모리: 148412 KB
-시간: 428 ms
+유기농 배추 / bfs
+작성일: 2025-07-25
+메모리: 114380 KB
+시간: 148 ms
 """
 import sys
-from collections import defaultdict
-n, m, r = map(int, sys.stdin.readline().strip().split())
-graph = defaultdict(list)
-for _ in range(m):
-    u, v = map(int, sys.stdin.readline().strip().split())
-    graph[u].append(v)
-    graph[v].append(u)
-for key in graph:
-    graph[key].sort()
+from collections import deque
+input = sys.stdin.readline
+directions = ([1,0],[0,1],[0,-1],[-1,0])
+t = int(input())
+cabbages = set()
+visited = set()
+total_visited_num = 0
 
-def dfs():
-    visited = [False] * (n + 1)
-    stack = [r]
-    order_list = []
-    while stack:
-        node = stack.pop()
-        if visited[node]:
-            continue
-        order_list.append(node)
-        visited[node] = True
-        for next in graph[node]:
-            if not visited[next]:
-                stack.append(next)
-    return order_list
-order_list = dfs()
-results = [0]*(n+1)
-for i in range(len(order_list)):
-    results[order_list[i]] = i+1
+def make_graph(k):
+    for _ in range(k):
+        x, y = map(int, input().strip().split())
+        cabbages.add((x, y))
 
-for result in results[1:]:
-    print(result)
+def bfs(start):
+    queue = deque([start])
+    cabbages.remove(start)
+    visited.add(start)
+    while queue:
+        x, y = queue.popleft()
+        for i, j in directions:
+            nx = x + i
+            ny = y + j
+            if nx >= 0 and ny >= 0 and nx < m and ny < n:
+                if (nx, ny) not in visited:
+                    if (nx, ny) in cabbages:
+                        queue.append((nx, ny))
+                        visited.add((nx,ny))
+                        cabbages.remove((nx, ny))
+    return len(visited)
+
+for _ in range(t):
+    cabbages.clear()
+    visited.clear()
+    visited_num = 0
+    count = 0
+    m, n, k = map(int, input().strip().split())
+    make_graph(k)
+    while visited_num != k:
+        visited_num = bfs(list(cabbages)[0])
+        count += 1
+    print(count)
