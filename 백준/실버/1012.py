8acf3e1ad45adcf71aed7d124fb472716d1302f0
Sussie <yurimc@student.unimelb.edu.au>
2025-07-25 13:59:10 +1000

    refactoring

--- 백준/실버/1012.py
+++ 백준/실버/1012.py
@@ -9,40 +9,26 @@ from collections import deque
 input = sys.stdin.readline
 directions = ([1,0],[0,1],[0,-1],[-1,0])
 t = int(input())
-cabbages = set()
-visited = set()
-total_visited_num = 0
 
-def make_graph(k):
-    for _ in range(k):
-        x, y = map(int, input().strip().split())
-        cabbages.add((x, y))
-
-def bfs(start):
+def bfs(start, cabbages):
     queue = deque([start])
     cabbages.remove(start)
-    visited.add(start)
     while queue:
         x, y = queue.popleft()
         for i, j in directions:
             nx = x + i
             ny = y + j
             if nx >= 0 and ny >= 0 and nx < m and ny < n:
-                if (nx, ny) not in visited:
-                    if (nx, ny) in cabbages:
-                        queue.append((nx, ny))
-                        visited.add((nx,ny))
-                        cabbages.remove((nx, ny))
-    return len(visited)
+                if (nx, ny) in cabbages:
+                    queue.append((nx, ny))
+                    cabbages.remove((nx, ny))
 
 for _ in range(t):
-    cabbages.clear()
-    visited.clear()
-    visited_num = 0
     count = 0
     m, n, k = map(int, input().strip().split())
-    make_graph(k)
-    while visited_num != k:
-        visited_num = bfs(list(cabbages)[0])
-        count += 1
+    cabbages = set(tuple(map(int, input().split())) for _ in range(k))
+
+    while cabbages:
+        bfs(list(cabbages)[0], cabbages)
+        count += 1  
     print(count)
