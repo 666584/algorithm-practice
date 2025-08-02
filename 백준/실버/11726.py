5b5c2aaaea590922b5b21936c952650ed91404d4
Sussie <yurimc@student.unimelb.edu.au>
2025-08-02 16:04:32 +1000

    dp

--- 백준/실버/11726.py
+++ 백준/실버/11726.py
@@ -1,10 +1,21 @@
 """
-작성일: 2025-07-31
-메모리:  KB
-시간:  ms
+2×n 타일링
+작성일: 2025-08-02
+메모리: 108384 KB
+시간:  96 ms
+
+dp[3] = 3
+dp[4] = 5
+dp[5] = 8
+dp[6] = 13
+dp[7] = 21 
 """
 import sys
 n = int(sys.stdin.readline())
-dp = []
+dp = [0]*1001
 dp[1] = 1
 dp[2] = 2
+
+for i in range(3, 1001):
+    dp[i] = dp[i-1]+dp[i-2]
+print(dp[n]%10007)
