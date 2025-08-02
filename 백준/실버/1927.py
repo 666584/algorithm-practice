82290a4edd558dd076c08a47e54088489e82aaca
Sussie <yurimc@student.unimelb.edu.au>
2025-08-02 16:27:22 +1000

    최소힙

--- 백준/실버/1927.py
+++ 백준/실버/1927.py
@@ -1,5 +1,21 @@
 """
-작성일: 2025-07-25
-메모리: 144232 KB
-시간: 180 ms
+작성일: 2025-08-02
+메모리: 114200 KB
+시간: 172 ms
 """
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+heap = []
+
+for _ in range(n):
+    new = int(sys.stdin.readline())
+    if new == 0:
+        if heap:
+            min_value = heapq.heappop(heap)
+            print(min_value)
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, new)
