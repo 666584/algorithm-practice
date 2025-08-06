d8541750bfbebf48bb582ba7d79407f4542bbfd5
Sussie <yurimc@student.unimelb.edu.au>
2025-08-06 14:11:09 +0900

    bfs

--- 백준/실버/1697.py
+++ 백준/실버/1697.py
@@ -1,30 +1,28 @@
 """
-NOT FINISHED
+bfs
+작성일: 2025-08-06
+메모리: 124768 KB
+시간: 168 ms
+
 n = start point
 k = goal
 walking: x-1 or x+1
 Teleporting: 2*x
 """
-5 - 4, 6, 10
-4 - 3, 7, 8
 import sys
 from collections import deque
-
+n, k = map(int, sys.stdin.readline().strip().split())
 def bfs(start, goal):
-    queue = deque([start])
-    visited = [start]
-    time = 0
+    queue = deque([(start, 0)])
+    visited = set([start])
     while queue:
-        number = queue.popleft()
-        print(number)
+        number, time = queue.popleft()
         if number == goal:
             return time
         moves = [number - 1, number + 1, number * 2]
         for move in moves:
             if move >= 0 and move <= 100000 and move not in visited:
-                time += 1
-                queue.append(move)
-                visited.append(move)
-    
-print(bfs(5, 17))
+                queue.append((move, time+1))
+                visited.add(move)   
+print(bfs(n, k))
         
