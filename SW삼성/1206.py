f9f5475a5fa1f5cfd933df9319e0cbeaf2eb279e
Yurim  Cho <joyulim320@gmail.com>
2025-11-14 13:08:35 +0900

    feat: 조망권 계산 로직 수정
    - 조망권 계산 방식을 '현재 빌딩 - 주변 4개 건물 중 최대 높이'로 변경
    - 주변 빌딩은 좌우 2칸씩 총 4개를 비교하여 가장 높은 건물 선택
    - 현재 빌딩 높이에서 최대 높이를 뺀 값을 조망권 세대수로 계산
    - 모든 빌딩에 대해 조망권 세대수를 합산하여 최종 결과 반환

--- SW삼성/1206.py
+++ SW삼성/1206.py
@@ -2,28 +2,29 @@ T = 10
 
 """
 한 빌딩에서 조망권이 좋은 세대 수:
-양옆으로(2개) 숫자를 빼서 그중 가장 작은 수
+현재 빌딩 - 주변 빌딩중 가장 세대수가 많은 빌딩
+이 빌딩들의 총합
 """
 def get_view_room(buildings, t):
     results = 0
     for i in range(2, t-2):
-        rooms = []
         pivot = buildings[i]
-        surroundings = [-1, -2, 1, 2]
-        for surrounding in surroundings:
-            room = pivot - buildings[i + surrounding]
-            if room >= 0:
-                rooms.append(room)
-        if rooms:
-            result = min(rooms)
-        else:
-            result = 0
-        results += result
+        surroundings = [buildings[i-1], buildings[i-2], buildings[i+1], buildings[i+2]]
+        min_surr = max(surroundings)
+        num_views = pivot - min_surr
+        if num_views < 0:
+            num_views = 0
+        results += num_views
     return results
 
+results = []
 for _ in range(T):
     t = int(input())
     buildings = list(map(int, input().strip().split()))
-    print(buildings)
     result = get_view_room(buildings, t)
-    print(result, "result")
+    results.append(result)
+
+i = 0
+for result in results:
+    print(f"#{i + 1} {result}")
+    i += 1
