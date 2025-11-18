5ff346b8ae849b4c45155663918817d634b87ebb
Yurim  Cho <joyulim320@gmail.com>
2025-11-18 13:43:23 +0900

    실행이 너무 오래 걸린다.

--- SW삼성/1244.py
+++ SW삼성/1244.py
@@ -1,45 +1,64 @@
-# unsolved
 t = int(input())
-def find_max_num(num_list, visited):
-    max_num = 0
-    max_i = 0
-    for i in range(len(num_list)):
-        x = num_list[i]
-        if i not in visited:
-            if x >= max_num:
-                max_num = x
-                max_i = i
-    return max_num, max_i
 
-def find_max_resut(num, N):
-    num = str(num)
-    num_list = []
-    visited = []
-    for a in num:
-        num_list.append(int(a))
-    for _ in range(N):
-        max_n, max_i = find_max_num(num_list, visited)
-        for i in range(len(num_list)):
-            if i != max_i and i not in visited:
-                prev = num_list[i]
-                num_list[i] = max_n
-                num_list[max_i] = prev
-                visited = []
-                visited.append(i)
-                visited.append(max_i)
-    return num_list
+def result_to_num(result):
+    res = 0
+    z = len(result) - 1
+    for number in result:
+        res += (number * (10**z))
+        z -= 1
+    return res
+
+def second_max_result(curr_list):
+    new_list = curr_list[:]
+    new_list[-1], new_list[-2] = new_list[-2], new_list[-1]
+    return new_list
+def new_max_finder(changed_i, changed_j, curr_list, n_filp, results, n, max_num, solved):
+    # 중복일 때를 고려해봐야 된다. 
+    if n_filp == 0:
+        return curr_list, False
+    
+    if max_num == result_to_num(curr_list):
+        if n_filp % 2 == 0:
+            return curr_list, True
+        else:
+            return second_max_result(curr_list), True
+ 
+    for i in range (0, len(curr_list) - 1):
+        for j in range(i+1, len(curr_list)):
+            if not changed_i and not changed_j: 
+                curr_list[i], curr_list[j] = curr_list[j], curr_list[i]
+                fin_list, stop = new_max_finder(i,j, curr_list, n_filp-1, results, n, max_num, solved)
+                if stop:
+                    return fin_list,True
+                results.append(fin_list[:])
+                curr_list[i], curr_list[j] = curr_list[j], curr_list[i]
+            elif i != changed_i or j != changed_j: 
+                curr_list[i], curr_list[j] = curr_list[j], curr_list[i]
+                fin_list, stop = new_max_finder(i,j, curr_list, n_filp-1, results, n, max_num, solved)
+                if stop:
+                    return fin_list,True
+                results.append(fin_list[:])
+                curr_list[i], curr_list[j] = curr_list[j], curr_list[i]
+    max_num = 0
+    max_list = []
+    for result in results:
+        res = result_to_num(result)
+        if res >= max_num:
+            max_num = res
+            max_list = result   
+    return max_list, False
 
 results = []
 for _ in range(t):
     num, n = map(int, input().strip().split())
-    results.append(find_max_resut(num, n))
+    num = str(num)
+    num_list = []
+    for a in num:
+        num_list.append(int(a))
+    max_list = []
+    max_num = result_to_num(sorted(num_list, reverse=True))
+    result, _ = new_max_finder(None, None, num_list, n, max_list, n, max_num, False)
+    results.append(result)
 
-z = 0
-for result in results:
-    res = 0
-    i = len(result) - 1
-    for number in result:
-        res += (number * (10**i))
-        i -= 1
-    print(f"#{z+1} {res}")
-    z += 1
+for i in range(t):
+    print(f"#{i+1} {result_to_num(results[i])}")
