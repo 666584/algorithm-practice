5f9ef043f06be7026de640368478561bcd7d5f0d
Yurim  Cho <joyulim320@gmail.com>
2025-11-18 15:19:12 +0900

    D3/solved

---
+++ SW삼성/5292.py
@@ -0,0 +1,7 @@
+T = int(input())
+for i in range(T):
+    n1, n2 = map(int, input().split())
+    set1 = set(map(int, input().split()))
+    set2 = set(map(int, input().split()))
+    result = len(set1& set2)
+    print(f"#{i+1} {result}")
