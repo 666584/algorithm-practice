e2c108b43f4a0aba10f1f338f86f96789a5299b7
Yurim  Cho <joyulim320@gmail.com>
2025-11-19 10:19:06 +0900

    refactoring

--- SW삼성/2805.py
+++ SW삼성/2805.py
@@ -1,41 +1,30 @@
-"""
-3 -> 1 
-5 -> 2 
-7 -> 3
-
-
-i = n // 2 next row -> i -= 1
-
-x = 1
-x += 2 until x = n 
-
-if x = n then 
-x -= 2 until x = 1
-"""
 T = int(input())
-for i in range(T):
+
+for t in range(1, T + 1):
     N = int(input())
-    matrix = []
-    for j in range(N):
-        num = input().strip()
-        num_list = [int(n) for n in num] 
-        matrix.append(num_list)
-    num_in_row = 1
-    start_num = N // 2
+    matrix = [list(map(int, input().strip())) for _ in range(N)]
+
     total = 0
-    row_num = 0
-    while num_in_row <= N:
-        for z in range(start_num, start_num + num_in_row):
-            total += matrix[row_num][z]
-        num_in_row += 2
-        row_num += 1
-        start_num -= 1
-    num_in_row = N - 2
-    start_num = 1
-    while num_in_row > 0:
-        for z in range(start_num, start_num + num_in_row):
-            total += matrix[row_num][z]
-        num_in_row -= 2
-        row_num += 1
-        start_num += 1
-    print(f"#{ i + 1} {total}")
+    center = N // 2
+
+    width = 1
+    start_col = center
+
+    row = 0
+    while width <= N:
+        total += sum(matrix[row][start_col : start_col + width])
+        width += 2
+        start_col -= 1
+        row += 1
+
+    # 아래쪽 삼각형
+    width = N - 2
+    start_col = 1
+
+    while width > 0:
+        total += sum(matrix[row][start_col : start_col + width])
+        width -= 2
+        start_col += 1
+        row += 1
+
+    print(f"#{t} {total}")
