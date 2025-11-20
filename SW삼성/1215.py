98808c4c8587791275dae9669fa8854f9fc836df
Yurim  Cho <joyulim320@gmail.com>
2025-11-20 15:01:53 +0900

    D3

--- SW삼성/1215.py
+++ SW삼성/1215.py
@@ -5,30 +5,33 @@ def is_p(word):
         if word[a] != word[-a-1]:
             return False
     return True
-for _ in range(10):
-    tc  = int(input())
+i = 0
+for tc in range(10):
+    num  = int(input())
     matrix = []
-    for j in range(100):
+    for j in range(8):
         matrix.append(list(s for s in input()))
-    max_score = 0
+    total_num = 0
     # 가로
-    for i in range(100):
-        for m in range(100):
-            for j in range(2, 101):
+    for i in range(8):
+        for m in range(8):
+            for j in range(2, 9):
                 word = []
                 word = matrix[i][m:j]
                 if is_p(word):
                     length = len(word)
-                    if length >= max_score:
-                        max_score = length
+                    if length == num:
+                        total_num += 1
     # 세로
-    for x in range(100):
-        for v in range(100):
-            for y in range(2, 101):
+    for x in range(8):
+        for v in range(8):
+            for y in range(2, 9):
                 word = []
                 word = [matrix[c][x] for c in range(v,y)]
                 if is_p(word):
                     length = len(word)
-                    if length >= max_score:
-                        max_score = length
-    print(f"#{tc} {max_score}")
+                    if length == num:
+                        total_num += 1
+    print(f"#{tc + 1} {total_num}")
+    #print(is_p("CCCCABAACCAABACCCC"))
+    #print(is_p('CC'))
