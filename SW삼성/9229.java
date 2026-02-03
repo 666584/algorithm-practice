700ff932928976fd2ab4b2d47b8c70a53ed89023
Yurim  Cho <joyulim320@gmail.com>
2026-02-03 15:22:48 +0900

    Refactor snack selection to use array instead of list
    - 일반 배열에 조기 종료를 넣었다. 
    - 일반 배열보다 메모리 사용량은 적으나 실행 시간이 길다.

--- SW삼성/9229.java
+++ SW삼성/9229.java
@@ -1,48 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class SWEA_9229_한빈이와_Sport_Mart_조유림 {
-	private static BufferedReader br;
-	
-	public static int twoPointer(int N, ArrayList<Integer> snacks, int M) {
-		Collections.sort(snacks);
-		
-		int max = -1;
-		int left = 0;
-		int right = snacks.size() - 1;
-
-		while (left < right) {
-		    int sum = snacks.get(left) + snacks.get(right);
-		    if (sum > M) right--;
-		    else {
-		        max = Math.max(max, sum);
-		        left++;
-		    }
-		}
-		return max;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int tc = Integer.parseInt(st.nextToken());
-		
-		for(int t = 0; t < tc; t++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			
-			st = new StringTokenizer(br.readLine());
-			ArrayList<Integer> snacks = new ArrayList<>();
-			for(int i = 0; i < N; i++) {
-				snacks.add(Integer.parseInt(st.nextToken()));
-			}
-			int result = twoPointer(N, snacks, M);
-			System.out.println("#"+(t+1)+" "+result);
-		}
-	}
+public class Main {
+    
+    public static int twoPointer(int N, int[] snackList, int M) {
+        Arrays.sort(snackList);
+        int max = -1;
+        int left = 0;
+        int right = N - 1;
+ 
+        while (left < right) {
+            int sum = snackList[left] + snackList[right];
+            // 조기 종료
+            if (sum == M) return M;
+            else if (sum > M) right--;
+            else {
+                max = Math.max(max, sum);
+                left++;
+            }
+        }
+         
+        return max;
+    }
+     
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int tc = Integer.parseInt(st.nextToken());
+         
+        for(int t = 0; t < tc; t++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+             
+            st = new StringTokenizer(br.readLine());
+            int[] snackList = new int[N];
+            for(int i = 0; i < N; i++) {
+                snackList[i] = Integer.parseInt(st.nextToken());
+            }
+            int result = twoPointer(N, snackList, M);
+            System.out.println("#"+(t+1)+" "+result);
+        }
+    }
 }
