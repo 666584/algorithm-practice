905bfcbc75bd38ae114d99c8093841c3b5eade58
Yurim  Cho <joyulim320@gmail.com>
2026-02-04 17:31:44 +0900

    Refactor card game logic and variable names

--- SW삼성/6808.java
+++ SW삼성/6808.java
@@ -5,15 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+/*
+ * 시간 복잡도: O(9!)
+ * 9! = 3265920
+ * */
 public class Main {
-	final static int SIZE = 9;
-	static BufferedReader br;
-	static int win;
-	static int lose;
+	private static final int CARD_COUNT = 9;
+    private static BufferedReader br;
+
+    private static int winCount;
+    private static int loseCount;
+    
+    /*
+     * 규영 카드 입력
+     * */
 	public static List<Integer> input(StringTokenizer st) {
 		
 		List<Integer> guyoung = new ArrayList<>();
-		for(int i = 0; i < SIZE; i++) {
+		for(int i = 0; i < CARD_COUNT; i++) {
 			guyoung.add(Integer.parseInt(st.nextToken())); 
 		}
 		
@@ -26,7 +35,7 @@ public class Main {
 	 * */
 	public static List<Integer> getYinyoung(List<Integer> guyoung) {
 		List<Integer> yinyoung = new ArrayList<>();
-		for(int i = 1; i < 18+1; i++) {
+		for(int i = 1; i <= 18; i++) {
 			if(!guyoung.contains(i)) yinyoung.add(i);
 		}
 		
@@ -34,43 +43,52 @@ public class Main {
 	}
 	
 	/*
+	 * 경기 결과 판정
 	 * 규영이가 지면 -1, 비기면 0, 이기면 1을 반환한다.
 	 * */
-	public static int isGuyoungWin(int[] cards, List<Integer> guyoung) {
-		int yTotal = 0;
-		int gTotal = 0;
-		for(int i = 0; i < 9; i++) {
-			int score = guyoung.get(i)+cards[i];
-			if(guyoung.get(i) > cards[i]) {
-				gTotal += score;
-			} else if(guyoung.get(i) < cards[i]) {
-				yTotal += score;
+	public static int compareScore(int[] yinyoungOrder, List<Integer> guyoung) {
+		int guyoungScore = 0;
+		int yinyoungScore = 0;
+
+		for(int i = 0; i < CARD_COUNT; i++) {
+			int sum = guyoung.get(i)+yinyoungOrder[i];
+			if(guyoung.get(i) > yinyoungOrder[i]) {
+				guyoungScore += sum;
+			} else if(guyoung.get(i) < yinyoungOrder[i]) {
+				yinyoungScore += sum;
 			}
 		}
 		
-		if(yTotal < gTotal) return 1;
-		else if (yTotal > gTotal) return -1;
+		if (guyoungScore > yinyoungScore) return 1;
+        if (guyoungScore < yinyoungScore) return -1;
 		return 0;
 	}
 	
+	/*
+	 * 완전 탐색(순열)
+	 * 인영 카드 순열 생성 및 결과 계산
+	 * */
 	// 완전탐색(순열)
-	public static void getWinAndLost(int cnt, boolean[] isSelected, int[] cards, List<Integer> yinyoung, List<Integer> guyoung) {
-		if (cnt == 9) {
-	        /*for (int card : cards) {
-	            System.out.print(card + " ");
-	        }*/
-	        int result = isGuyoungWin(cards, guyoung);
-	        //System.out.println(result);
-	        if(result == 1) win++;
-	        else if(result == -1) lose++;
+	public static void permute(
+			int depth, 
+			boolean[] isSelected, 
+			int[] yinyoungOrder, 
+			List<Integer> yinyoung, 
+			List<Integer> guyoung) {
+		
+		if (depth == CARD_COUNT) {
+	        int result = compareScore(yinyoungOrder, guyoung);
+	        if(result == 1) winCount++;
+	        else if(result == -1) loseCount++;
 	        return;
 	    }
 
-	    for (int i = 0; i < 9; i++) {
-	        if (isSelected[i]) continue;	        
-	        cards[cnt] = yinyoung.get(i);
+	    for (int i = 0; i < CARD_COUNT; i++) {
+	        if (isSelected[i]) continue;
+	        
+	        yinyoungOrder[depth] = yinyoung.get(i);
 	        isSelected[i] = true;
-	        getWinAndLost(cnt + 1, isSelected, cards, yinyoung, guyoung);	        
+	        permute(depth + 1, isSelected, yinyoungOrder, yinyoung, guyoung);	        
 	        isSelected[i] = false;
 	    }
 	    
@@ -78,18 +96,23 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		for(int i = 1; i < N+1; i++) {
-			win = 0;
-			lose = 0;
+		int testCase = Integer.parseInt(st.nextToken());
+		
+		for(int t = 1; t <= testCase; t++) {
+			winCount = 0;
+			loseCount = 0;
+			
 			st = new StringTokenizer(br.readLine());
 			List<Integer> guyoung = input(st);
 			List<Integer> yinyoung = getYinyoung(guyoung);
-			boolean[] isSelected = new boolean[9];
-			int[] cards = new int[9];
-			getWinAndLost(0, isSelected, cards, yinyoung, guyoung);
-			System.out.println("#"+i+" "+win+" "+lose);
+			
+			boolean[] isSelected = new boolean[CARD_COUNT];
+			int[] cards = new int[CARD_COUNT];
+			
+			permute(0, isSelected, cards, yinyoung, guyoung);
+			System.out.println("#"+t+" "+winCount+" "+loseCount);
 		}
 	}
 }
