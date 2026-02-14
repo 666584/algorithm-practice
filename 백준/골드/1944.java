5a1e38a7e59714903b4ea3501e6d4ca21a5fc3f0
Yurim  Cho <joyulim320@gmail.com>
2026-02-14 10:27:53 +0900

    Refactor BJ_1944_복제로봇
    - BFS 호출 로직을 바꾸어 하나의 정점에서 모든 정점에 대한 bfs를 한다. 
    - -1을 리턴하는 경우를 구현(모든 정점과 연결이 되지 않을 경우)

--- 백준/골드/1944.java
+++ 백준/골드/1944.java
@@ -1,18 +1,3 @@
-/*
-    시작 위치가 정해져 있다.
-    N*N의 정사각형 미로, M개의 흩어진 열쇠의 위치.
-    모든 열쇠를 찾으면서 로볼이 움직이는 횟수의 합을 최소로하는 프로그램 ㅣ작성.
-    상하좌우 사방 탐색.
-    로봇 수를 마음대로 가질 수 이싸.
-    로봇이 움직이는 횟수의 합은 분열된 로봇 각각이 움직인 횟수의 총합.
-    열쇠가 있는 곳들과 로봇이 출발하는 위치에 로봇이 복제 할 수 있따.
-
-    1. 우선 각 위치에서 다른 위치로 가는 가장 짧은 거리 즉, 가중치를 구해야 한다.
-    1-1. 가중치를 구하기 위해선 bfs를 사용한다.
-    2. 가중치를 구했으면 그냥 mst 말고 무조건 시작 지점(가장 짧은 거리) 부터 시작하는 mst를 하면 된다.
-    끝!
- */
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,27 +6,18 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-/*
-* 입력:
-* N, M 미로 크기, 열쇠 개수(정점수-1)
-* 1은 미로의 벽, 0은 지나다닐 수 있는 길.
-* S는 로봇이 출발하는 위치, K는 열쇠의 위치
-* S는 1개, K는 M개
-* 모든 테두리는 벽이다.
-* */
-public class BJ_1944_복제로봇_조유림 {
-    static int N, M; //미로 크기, 열쇠 개수
-    static int[][] distances; //간선의 정보를 저장한다. (시작점의 인덱스는 간선개수이다.)
-    static int[][] map; // 로봇이 출발하는 위치는 2로, K는 3으로 표시한다.
-    static int startX, startY; // 시작점 좌표
-    static int[][] keys; //열쇠 좌표
+public class Main {
+    static int N, M;
+    static int[][] distances;
+    static int[][] map;
+    static int startX, startY;
+    static int[][] keys;
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0,};
-    static int totalEdges; //distances의 개수, 총 간선 개수
+    static int totalEdges;
     static int[] parent;
-    static int totalCost; //로봇이 움직인 총 횟수.
-    static int edgeIndex; // 간선의 인덱스 업데이트를 위함.
-
+    static int totalCost;
+    static int[][] edges;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -60,7 +36,7 @@ public class BJ_1944_복제로봇_조유림 {
                 } else if (line.charAt(j) == 'K') {
                     keys[cnt][0] = i;
                     keys[cnt][1] = j;
-                    map[i][j] = 3;
+                    map[i][j] = 3+cnt;
                     cnt++;
                 } else map[i][j] = line.charAt(j) - '0';
             }
@@ -68,43 +44,46 @@ public class BJ_1944_복제로봇_조유림 {
 
         int[] start = {startX, startY};
 
-
-        totalEdges = 0;
-        for (int i = M; i > 0; i--) {
-            totalEdges += i;
-        }
-        distances = new int[totalEdges][3];
-        edgeIndex = 0;
-        for(int i = 0; i < M; i++){
-            int cost = bfs(start, keys[i]);
-            distances[edgeIndex][0] = M;
-            distances[edgeIndex][1] = i;
-            distances[edgeIndex][2] = cost;
-            edgeIndex++;
-        }
+        distances = new int[M+1][M+1];
+        for (int i=0;i<M+1;i++) Arrays.fill(distances[i], -1);
+        bfs(start, M);
         findDistance();
-        Arrays.sort(distances, (x, y) -> Integer.compare(x[2], y[2]));
         totalCost = 0;
         parent = new int[M+1];
+        totalEdges = 0;
         for(int i = 0; i < M+1; i++){
             parent[i] = i;
+            totalEdges += i;
         }
-        mst();
+
+        edges = new int[totalEdges][3];
+        int a = 0;
+        for(int i = 0; i < M+1; i++){
+            for(int j = 0; j < M+1; j++){
+                if(distances[i][j] != -1 && i < j){
+                    edges[a][0] = i;
+                    edges[a][1] = j;
+                    edges[a][2] = distances[i][j];
+                    a++;
+                }
+            }
+        }
+        if(a != totalEdges){
+            System.out.println(-1);
+        }else{
+            Arrays.sort(edges, (x,y)->Integer.compare(x[2], y[2]));
+            mst();
+            System.out.println(totalCost);
+        }
     }
 
     public static void findDistance() {
         for (int i = 0; i < M; i++) {
-            for (int j = i + 1; j < M; j++) {
-                int cost = bfs(keys[i], keys[j]);
-                distances[edgeIndex][0] = i;
-                distances[edgeIndex][1] = j;
-                distances[edgeIndex][2] = cost;
-                edgeIndex++;
-            }
+            bfs(keys[i], i);
         }
     }
 
-    public static int bfs(int[] start, int[] end) {
+    public static void bfs(int[] start, int startIndex) {
         Deque<int[]> queue = new ArrayDeque<>();
         int[] startPoint = {start[0], start[1], 0};
         queue.add(startPoint);
@@ -115,23 +94,26 @@ public class BJ_1944_복제로봇_조유림 {
             int cx = curr[0];
             int cy = curr[1];
             int cost = curr[2];
-            if (cx == end[0] && cy == end[1]) {
-                return cost;
+            if(map[cx][cy] >= 3){
+                distances[startIndex][map[cx][cy]-3] = cost;
             }
-            if (visited[cx][cy]) continue;
-            visited[curr[0]][curr[1]] = true;
+            if (map[cx][cy] == 2){
+                distances[startIndex][M] = cost;
+            }
             for (int i = 0; i < 4; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
                 if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                    if (map[nx][ny] == 0 || map[nx][ny] == 3) {
-                        int[] next = {nx, ny, cost + 1};
-                        queue.addLast(next);
+                    if (!visited[nx][ny]) {
+                        if (map[nx][ny] != 1) {
+                            int[] next = {nx, ny, cost + 1};
+                            visited[nx][ny] = true;
+                            queue.addLast(next);
+                        }
                     }
                 }
             }
         }
-        return 0;
     }
     public static int find(int x){
         if(parent[x] != x){
@@ -150,9 +132,9 @@ public class BJ_1944_복제로봇_조유림 {
     }
     public static void mst(){
         for(int i = 0; i < totalEdges; i++){
-            int a = distances[i][0];
-            int b = distances[i][1];
-            int cost = distances[i][2];
+            int a = edges[i][0];
+            int b = edges[i][1];
+            int cost = edges[i][2];
             if(find(a) != find(b)){
                 union(a, b);
                 totalCost += cost;
