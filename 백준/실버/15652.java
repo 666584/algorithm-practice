92649d9caba700a4fa4fb50990755a60d435a00f
Yurim  Cho <joyulim320@gmail.com>
2026-02-16 20:45:54 +0900

    Update 15652.java

--- 백준/실버/15652.java
+++ 백준/실버/15652.java
@@ -13,24 +13,19 @@ public class BJ_15652_N과M4 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         number = new int[M+1];
-        combination(1);
+        combination(0, 1);
     }
 
     //조합
-    public static void combination(int start){
-        if(start == M+1) {
-            for(int i = 1; i <= M; i++){
-                System.out.print(number[i]+" ");
-            }
+    public static void combination(int depth, int startVal){
+        if(depth == M){
+            for(int i=0;i<M;i++) System.out.print(number[i] + " ");
             System.out.println();
             return;
         }
-
-        for(int i = 1; i <= N; i++){
-            if(start > 0 && number[start-1] > i) continue;
-            number[start] = i;
-            combination(start+1);
-            //System.out.println(start);
+        for(int i=startVal;i<=N;i++){
+            number[depth] = i;
+            combination(depth+1, i);
         }
     }
 }
