18e6e2127b0eef621a8c01a1c39e4b7385c46869
Yurim  Cho <joyulim320@gmail.com>
2026-02-19 15:22:11 +0900

    Create 13549.java
    0-1 BFS

---
+++ 백준/골드/13549.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_13549_숨바꼭질3_조유림{
+	static int N, K; 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		bfs();
+	}
+	
+	public static void bfs() {
+		ArrayDeque<int[]> deque = new ArrayDeque<>();
+		deque.add(new int[] {N, 0});
+		boolean[] visited = new boolean[100001];
+		while(!deque.isEmpty()) {
+			int[] curr = deque.poll();
+			
+			int num = curr[0];
+			int cost = curr[1];
+			//System.out.println(num);
+			if(num == K) {
+				System.out.println(cost);
+				return;
+			}
+			
+			if (num*2 <= 100000 && !visited[num*2]) { 
+				deque.addFirst(new int[] {num*2, cost}); 
+				visited[num*2] = true;
+			}
+			if (num+1 <= 100000 && !visited[num+1]) { 
+				deque.addLast(new int[] {num+1, cost+1}); 
+				visited[num+1] = true;
+			}
+			if (num-1 >= 0 && !visited[num-1]) {
+				deque.addLast(new int[] {num-1, cost+1});
+				visited[num-1] = true;
+			}
+
+		}
+	}
+}
